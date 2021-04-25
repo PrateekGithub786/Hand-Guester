cfd1be8957164c1d47bc702f4e580acd18691864
PrateekGithub786 <75919841+PrateekGithub786@users.noreply.github.com>
2021-04-25 08:48:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function takeSnapshot(){
 
 console.log('ml5 version', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8lb4_xTww/', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json', modelLoaded);
 
 function modelLoaded(){
     console.log("model is loaded");
@@ -33,3 +33,42 @@ function speak(){
     var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
     synth.speak(utterThis);
 }
+
+function showPrediction(){
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction1 = results[0].label;
+        prediction2 = results[1].label;
+        speak();
+
+        if(prediction1 == "happy"){
+            document.getElementById("update_emoji").innerHTML = "&#128522;";
+        }
+        if(prediction1 == "sad"){
+            document.getElementById("update_emoji").innerHTML = "&#128532;";
+        }
+        if(prediction1 == "angry"){
+            document.getElementById("update_emoji").innerHTML = "&#128548;";
+        }
+
+        if(prediction2 == "happy"){
+            document.getElementById("update_emoji2").innerHTML = "&#128522;";
+        }
+        if(prediction2 == "sad"){
+            document.getElementById("update_emoji2").innerHTML = "&#128532;";
+        }
+        if(prediction2 == "angry"){
+            document.getElementById("update_emoji2").innerHTML = "&#128548;";
+        }
+    }
+}
+
